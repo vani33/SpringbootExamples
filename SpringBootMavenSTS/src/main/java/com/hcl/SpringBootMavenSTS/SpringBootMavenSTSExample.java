bfec1f31ce62c51f965f433deaf7996e90a7d70d
vani33 <59596782+vani33@users.noreply.github.com>
2021-03-12 13:33:34 +0530

    Springboot Example first commit

---
+++ SpringBootMavenSTS/src/main/java/com/hcl/SpringBootMavenSTS/SpringBootMavenSTSExample.java
@@ -0,0 +1,13 @@
+package com.hcl.SpringBootMavenSTS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMavenSTSExample {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBootMavenSTSExample.class, args);
+	}
+}
